42db140da84d4c99c52c2fcbde8c214f3fbd5d45
MMASDEH <muhcis@gmail.com>
2024-07-06 14:54:02 +0400

    update services

--- src/services/quote.service.ts
+++ src/services/quote.service.ts
@@ -4,6 +4,8 @@ import { IQuote } from "../interfaces/quote.interface";
 const url = 'https://dummyjson.com/quotes'
 export const quoteService = {
     getAll: () => {
-       return axiosInstance.get<IQuote>(url).then(d => d);
+        return axiosInstance.get<IQuote>(url).then(d => d).catch(err => {
+            console.log(err.message)
+        });
     }
 }
--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -2,10 +2,14 @@ import { axiosInstance } from "../core/interceptor"
 import { IUsers } from "../interfaces/user.interface";
 
 
-const url='https://dummyjson.com/users';
-export const userService={
-    getAll: async ()=>{
-        return await axiosInstance.get<IUsers>(url).then(d=>d);
+const url = 'https://dummyjson.com/users';
+export const userService = {
+    getAll: async () => {
+        return (await axiosInstance.get<IUsers>(url, {
+            headers: {
+                "Content-Type": "application/josn"
+            }
+        }).then(d => d))
     }
-    
+
 }
